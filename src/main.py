d573cd536745fd12c9c48b0318072731e131aca1
Saparmurat <saparmurat@protonmail.com>
2024-02-27 01:07:10 +0600

    include inventory router

--- src/main.py
+++ src/main.py
@@ -4,7 +4,9 @@ from pydantic import BaseModel, HttpUrl
 
 from fastapi import FastAPI, Query, Path
 
-from .config import settings
+from src.config import settings
+
+from src.inventory.router import router
 
 app = FastAPI()
 
@@ -24,12 +26,12 @@ class Item(BaseModel):
 
 
 @app.get("/")
-async def root():
+def root():
     return {"message": "Hello World"}
 
 
 @app.put("/items_foo/{item_id}")
-async def items_foo(
+def items_foo(
     item_id: Annotated[int, Path(title="Unique identifier", ge=0, le=1000)],
     q: Annotated[str | None, Query(max_length=32)] = None,
     item: Item | None = None,
@@ -46,5 +48,8 @@ async def items_foo(
 
 
 @app.get("/env/")
-async def env() -> dict:
+def env() -> dict:
     return settings.model_dump()
+
+
+app.include_router(router, prefix="/inventory", tags=["Inventory"])
