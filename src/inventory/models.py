30bc770c6230ed7ad895415edbb0562b67bdd0d5
Saparmurat <saparmurat@protonmail.com>
2024-02-27 01:06:01 +0600

    add models

---
+++ src/inventory/models.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Column, String, Integer, ForeignKey, Float, Time
+from sqlalchemy.orm import relationship
+from src.database import Base
+
+
+class Establishment(Base):
+    __tablename__ = "establishments"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, index=True)
+    description = Column(String)
+    # location = Column(String)
+    # opening_hours = Column(Time)
+
+    items = relationship("Item", back_populates="establishment")
+
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, index=True)
+    description = Column(String)
+    price = Column(Float)
+
+    establishment_id = Column(Integer, ForeignKey("establishments.id"))
+
+    establishment = relationship("Establishment", back_populates="items")
