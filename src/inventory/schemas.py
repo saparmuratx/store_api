c96613f1e35ad6bcaf50cea88c76a1f72ba42ef3
Saparmurat <saparmurat@protonmail.com>
2024-02-28 01:14:35 +0600

    add fields

--- src/inventory/schemas.py
+++ src/inventory/schemas.py
@@ -6,6 +6,7 @@ class ItemBase(BaseModel):
     name: str
     description: str
     price: float
+    quantity: int
 
 
 class ItemCreate(ItemBase):
@@ -23,6 +24,8 @@ class Item(ItemBase):
 class EstablishmentBase(BaseModel):
     name: str
     description: str
+    location: str
+    opening_hours: time
 
 
 class EstablishtmentCreate(EstablishmentBase):
