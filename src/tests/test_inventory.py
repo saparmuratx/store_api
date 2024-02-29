00f38e2624cca582cdbc32aed80a855dc4cde096
snurlan <snurlan@nurtelecom.kg>
2024-02-29 17:06:00 +0600

    add tests for items

--- src/tests/test_inventory.py
+++ src/tests/test_inventory.py
@@ -1,8 +1,31 @@
-import json
-
 from fastapi.testclient import TestClient
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
+
+from src.database import Base, get_db
 from src.main import app
+from src.config import settings
+
+engine = create_engine(settings.TEST_DATABASE_URL, poolclass=StaticPool)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+Base.metadata.create_all(bind=engine)
+
+
+def override_get_db():
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+app.dependency_overrides[get_db] = override_get_db
+
+client = TestClient(app)
 
 
 client = TestClient(app)
@@ -41,7 +64,7 @@ def test_create_est():
     assert response.json() == test_response
 
 
-def test_creat_invalid_est():
+def test_create_invalid_est():
     test_data = {
         "name": "Derinkuyu",
         "description": "Templar HQ",
@@ -52,3 +75,41 @@ def test_creat_invalid_est():
     response = client.post("/inventory/establishments", json=test_data)
 
     assert response.status_code == 422
+
+
+def test_create_item():
+    test_data = {
+        "name": "Yatagan",
+        "description": "Ottoman sword, imported from Istanbul",
+        "price": 420,
+        "quantity": 69,
+    }
+
+    test_response = {
+        "name": "Yatagan",
+        "description": "Ottoman sword, imported from Istanbul",
+        "price": 420.0,
+        "quantity": 69,
+        "id": 1,
+        "establishment_id": 1,
+    }
+
+    response = client.post("/inventory/establishments/1/items/", json=test_data)
+
+    assert response.status_code == 200
+
+    assert response.json() == test_response
+
+
+def test_create_invalid_item():
+    test_data = {
+        "description": "Ottoman sword, imported from Istanbul",
+        "price": 420,
+        "quantity": 69,
+    }
+
+    response = client.post('inventory/establishments/1/items/', json=test_data)
+
+    assert response.status_code == 422
+
+    
