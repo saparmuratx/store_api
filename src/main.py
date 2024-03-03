08c5ceb5ec8d6ccd59cd3ad34ad8179fa5a64661
Saparmurat <saparmurat@protonmail.com>
2024-03-03 22:04:40 +0600

    remove unnecessary routes

--- src/main.py
+++ src/main.py
@@ -1,55 +1,8 @@
-from typing import Annotated
-
-from pydantic import BaseModel, HttpUrl
-
-from fastapi import FastAPI, Query, Path
-
-from src.config import settings
+from fastapi import FastAPI
 
 from src.inventory.router import router
 
+
 app = FastAPI()
-
-
-class Image(BaseModel):
-    url: HttpUrl
-    name: str
-
-
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
-
-    image: Image | None = None
-
-
-@app.get("/")
-def root():
-    return {"message": "Hello World"}
-
-
-@app.put("/items_foo/{item_id}")
-def items_foo(
-    item_id: Annotated[int, Path(title="Unique identifier", ge=0, le=1000)],
-    q: Annotated[str | None, Query(max_length=32)] = None,
-    item: Item | None = None,
-) -> dict:
-    results = {"item_id": item_id}
-
-    if q:
-        results.update({"q": q})
-
-    if item:
-        results.update({"item": item})
-
-    return results
-
-
-@app.get("/env/")
-def env() -> dict:
-    return settings.model_dump()
-
 
 app.include_router(router, prefix="/inventory", tags=["Inventory"])
