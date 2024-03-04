f971e7a6e5b37b93fc6ad81839a02815b4012b3f
Saparmurat <saparmurat@protonmail.com>
2024-03-04 22:07:09 +0600

    fix list items

--- src/inventory/crud.py
+++ src/inventory/crud.py
@@ -69,7 +69,13 @@ def read_item(db: Session, item_id: int):
 
 
 def read_items(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.Item).offset(skip).limit(limit).all()
+    return (
+        db.query(models.Item)
+        .filter(models.Item.establishment_id.is_not(None))
+        .offset(skip)
+        .limit(limit)
+        .all()
+    )
 
 
 def create_item(db: Session, item: schemas.ItemCreate, establishment_id):
