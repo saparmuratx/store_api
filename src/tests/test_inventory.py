0e509a79ccd87fa26fe8ddf202cfb2ffbf5b30b2
Saparmurat <saparmurat@protonmail.com>
2024-03-04 22:06:51 +0600

    add delete, update test cases

--- src/tests/test_inventory.py
+++ src/tests/test_inventory.py
@@ -8,6 +8,7 @@ from src.database import Base, get_db
 from src.main import app
 from src.config import settings
 
+
 engine = create_engine(settings.TEST_DATABASE_URL, poolclass=StaticPool)
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
@@ -24,9 +25,6 @@ def override_get_db():
 
 
 app.dependency_overrides[get_db] = override_get_db
-
-client = TestClient(app)
-
 
 client = TestClient(app)
 
@@ -91,6 +89,43 @@ def test_create_item():
     assert response.json() == test_response
 
 
+def test_update_item():
+    test_data = {
+        "description": "Ottoman sword, imported from Istanbul(only 2 left)",
+        "price": 210,
+        "quantity": 2,
+    }
+
+    test_response = {
+        "name": "Yatagan",
+        "description": "Ottoman sword, imported from Istanbul(only 2 left)",
+        "price": 210.0,
+        "quantity": 2,
+        "id": 1,
+        "establishment_id": 1,
+    }
+
+    response = client.put("/inventory/items/1", json=test_data)
+
+    print(response.json())
+
+    assert response.status_code == 200
+
+    assert response.json() == test_response
+
+
+def test_update_item_invalid():
+    test_data = {
+        "description": "Ottoman sword, imported from Istanbul(only 2 left)",
+        "price": 210,
+        "quantity": "a lot of them",
+    }
+
+    response = client.post("/inventory/establishments/1/items/", json=test_data)
+
+    assert response.status_code == 422
+
+
 def test_create_invalid_item():
     test_data = {
         "description": "Ottoman sword, imported from Istanbul",
@@ -101,3 +136,19 @@ def test_create_invalid_item():
     response = client.post("inventory/establishments/1/items/", json=test_data)
 
     assert response.status_code == 422
+
+
+def test_delete_invalide_item():
+    test_response = {"detail": "Item not found"}
+
+    response = client.delete("inventory/establishments/32/")
+
+    assert response.status_code == 404
+
+    assert response.json() == test_response
+
+
+def test_delete_item():
+    response = client.delete("inventory/establishments/1/")
+
+    assert response.status_code == 204
