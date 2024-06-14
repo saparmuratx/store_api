39bbd5abc941c263e4987b5b4bf5bf5a7e603ccb
snurlan <snurlan@nurtelecom.kg>
2024-06-14 18:38:19 +0600

    add routes

--- src/auth/router.py
+++ src/auth/router.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 
 from . import models, schemas, crud
 
@@ -13,4 +13,20 @@ auth_router = APIRouter()
 
 @auth_router.post("/register/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    return crud.create_user(db, user)
+    try:
+        db_user = crud.create_user(db, user)
+
+    except KeyError as err:
+        raise HTTPException(status_code=400, detail=str(err))
+
+    return db_user
+
+
+@auth_router.get("/users/{user_id}", response_model=schemas.User)
+def read_user_by_id(user_id: int, db: Session = Depends(get_db)):
+    user = crud.read_user_by_id(db, user_id)
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    return user
