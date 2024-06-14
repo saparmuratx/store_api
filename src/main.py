69a2469e7a81eccaf0e124e71d462249f574d4d6
snurlan <snurlan@nurtelecom.kg>
2024-06-14 11:49:03 +0600

    add auth_router

--- src/main.py
+++ src/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
 from src.inventory.router import router
-
+from src.auth.router import auth_router
 
 app = FastAPI()
 
-app.include_router(router, prefix="/inventory", tags=["Inventory"])
+app.include_router(router, prefix="/api/inventory", tags=["Inventory"])
+app.include_router(auth_router, prefix="/api/auth", tags=["Auth"])
+
+
+@app.get("/")
+async def home_redirect():
+    return RedirectResponse(url="/docs", status_code=302)
