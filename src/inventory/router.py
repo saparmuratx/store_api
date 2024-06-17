60a9ea1906cd6f6628912d8eb349b50554ab2858
Saparmurat <saparmurat@protonmail.com>
2024-06-18 04:10:43 +0600

    temporary rename to vendors

--- src/inventory/router.py
+++ src/inventory/router.py
@@ -1,7 +1,11 @@
+from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from . import crud, models, schemas
+
+from src.auth.schemas import User
+from src.auth.router import get_current_active_user
 
 from src.database import get_db, engine
 
@@ -10,67 +14,82 @@ models.Base.metadata.create_all(bind=engine)
 router = APIRouter()
 
 
-@router.post("/establishments/", response_model=schemas.EstablishmentCreateResponse)
-def create_establishment(
-    establishment: schemas.EstablishtmentCreate, db: Session = Depends(get_db)
+@router.post("/vendors/", response_model=schemas.EstablishmentCreateResponse)
+def create_vendor(
+    establishment: schemas.EstablishmentCreate,
+    current_user: Annotated[User, Depends(get_current_active_user)],
+    db: Session = Depends(get_db),
 ):
-    return crud.create_establishment(db=db, establishment=establishment)
+    return crud.create_establishment(
+        db=db, establishment=establishment, user_id=current_user.id
+    )
 
 
-@router.get("/establishments/{establishment_id}/", response_model=schemas.Establishment)
-def get_establishment(establishment_id: int, db: Session = Depends(get_db)):
-    establishment = crud.read_establishment(db=db, establishment_id=establishment_id)
+@router.get("/vendors/{vendor_id}/", response_model=schemas.Establishment)
+def get_vendor(vendor_id: int, db: Session = Depends(get_db)):
+    establishment = crud.read_establishment(db=db, establishment_id=vendor_id)
 
     if not establishment:
-        raise HTTPException(status_code=404, detail="Establishment not found")
+        raise HTTPException(status_code=404, detail="Vendor not found")
 
     return establishment
 
 
-@router.get("/establishments/", response_model=list[schemas.Establishment])
+@router.get("/vendors/", response_model=list[schemas.Establishment])
 def read_establishmetns(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return crud.read_establishments(db=db, skip=skip, limit=limit)
 
 
 @router.put(
-    "/establishments/{establishment_id}/",
+    "/vendors/{vendor_id}/",
     response_model=schemas.EstablishmentUpdateResponse,
 )
-def update_establishment(
-    establishment_id: int,
+def update_vendor(
+    vendor_id: int,
     establishment: schemas.EstablishmentUpdate,
+    current_user: Annotated[User, Depends(get_current_active_user)],
     db: Session = Depends(get_db),
 ):
-    establishment = crud.update_establishment(db, establishment, establishment_id)
+    establishment = crud.update_establishment(
+        db, establishment, vendor_id, current_user.id
+    )
 
     return establishment
 
 
-@router.delete("/establishments/{establishment_id}/", status_code=204)
-def delete_establishment(establishment_id: int, db: Session = Depends(get_db)):
-    establishment = crud.read_establishment(db, establishment_id)
+@router.delete("/vendors/{vendor_id}/", status_code=204)
+def delete_vendor(vendor_id: int, db: Session = Depends(get_db)):
+    establishment = crud.read_establishment(db, vendor_id)
 
     if not establishment:
         raise HTTPException(status_code=404, detail="Item not found")
 
-    crud.delete_establishment(db, establishment_id)
+    crud.delete_establishment(db, vendor_id)
 
     return None
 
 
-@router.post("/establishments/{establishment_id}/items/", response_model=schemas.Item)
+@router.post("/vendors/{vendor_id}/items/", response_model=schemas.Item)
 def create_item(
-    establishment_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
+    vendor_id: int,
+    item: schemas.ItemCreate,
+    current_user: Annotated[User, Depends(get_current_active_user)],
+    db: Session = Depends(get_db),
 ):
-    establishment = crud.read_establishment(db, establishment_id)
+    establishment = crud.read_establishment(db, vendor_id)
 
     if not establishment:
         raise HTTPException(
             status_code=404,
-            detail=f"Establishment not found with ID {establishment_id}",
+            detail=f"Vendor not found with ID {vendor_id}",
         )
 
-    return crud.create_item(db, item=item, establishment_id=establishment_id)
+    if establishment.user_id != current_user.id:
+        raise crud.no_permission_error
+
+    return crud.create_item(
+        db, item=item, establishment_id=vendor_id, user_id=current_user.id
+    )
 
 
 @router.get("/items/", response_model=list[schemas.Item])
@@ -90,19 +109,28 @@ def get_item(item_id: int, db: Session = Depends(get_db)):
 
 
 @router.put("/items/{item_id}/", response_model=schemas.Item)
-def update_item(item_id: int, item: schemas.ItemUpdate, db: Session = Depends(get_db)):
-    updated_item = crud.update_item(db, item, item_id)
+def update_item(
+    item_id: int,
+    item: schemas.ItemUpdate,
+    current_user: Annotated[User, Depends(get_current_active_user)],
+    db: Session = Depends(get_db),
+):
+    updated_item = crud.update_item(db, item, item_id, current_user.id)
 
     return updated_item
 
 
 @router.delete("/items/{item_id}/", status_code=204)
-def delete_item(item_id: int, db: Session = Depends(get_db)):
+def delete_item(
+    item_id: int,
+    current_user: Annotated[User, Depends(get_current_active_user)],
+    db: Session = Depends(get_db),
+):
     item = crud.read_item(db, item_id)
 
     if not item:
         raise HTTPException(status_code=404, detail="Item not found")
 
-    crud.delete_item(db, item_id)
+    crud.delete_item(db, item_id, current_user.id)
 
     return None
