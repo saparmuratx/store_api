12a0786bada2e2db65d274706eb57c61f72d8f53
snurlan <snurlan@nurtelecom.kg>
2024-06-18 18:36:30 +0600

    add api description

--- src/main.py
+++ src/main.py
@@ -4,7 +4,11 @@ from fastapi.responses import RedirectResponse
 from src.inventory.router import router
 from src.auth.router import auth_router
 
-app = FastAPI()
+app = FastAPI(
+    title="Bazar API",
+    description="API for Vendors and Items",
+    version="0.6.9",
+)
 
 app.include_router(auth_router, prefix="/api/auth", tags=["Auth"])
 app.include_router(router, prefix="/api/inventory", tags=["Inventory"])
